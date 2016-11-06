3dde244d376f42525b6ea94d47d52942977f04fa
tangyiwen <fa33053@gmail.com>
2016-11-06 16:04:12 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,23 +13,63 @@ $(document).ready(function(){
   });
 
   $(".habrgr > span").click(function(){   /*漢堡選單*/
-    if( $(this).hasClass("click") ){
-      $(this).removeClass("click");
+    if( $("nav").hasClass("click") ){
+      $("nav").removeClass("click");
       $("nav  ul").slideUp();
     }else{
-      $(this).addClass("click");
+      $("nav").addClass("click");
       $("nav  ul").slideDown();
       return false;
     }
   });
+  $("nav").mouseleave(function(){
+    if( $(this).hasClass("click") ){
+      $("nav ul").slideUp();
+      $(this).removeClass("click");
+    }
 
-  $("content a").hover(function(){
+  });
+
+  $("content a").hover(function(){        /*透明片*/
     $(this).append("<div></div>");
     $(this).children("div").addClass("mask");
+    var img_height = $(this).children("img").height();
+    $(".mask").height( img_height );
   });
   $("content a").mouseleave(function(){
     $(this).children("div").remove();
   });
 
+
+
+  $(".show_img img").hover(function(){  /*大型 幻燈片*/
+    var img_src = $(this).attr("src");
+    $(".show_box").attr("src",img_src);
+
+  });
+
+  $(".buttomR").click(function(){   /*  S 幻燈片 */
+    var $old =$(".point");
+    if( $(".point").hasClass("last") ){
+      $(".first").addClass("point");
+    }else{
+      $(".point").next().addClass("point");
+    }
+    $old.removeClass("point");
+    var point_src = $(".point").attr("src");
+    $(".show_box").attr("src",point_src);
+  });
+  $(".buttomL").click(function(){   /*  S 幻燈片 */
+    var $old =$(".point");
+    if( $(".point").hasClass("first") ){
+      $(".last").addClass("point");
+    }else{
+      $(".point").prev().addClass("point");
+    }
+    $old.removeClass("point");
+    var point_src = $(".point").attr("src");
+    $(".show_box").attr("src",point_src);
+
+  });
 
 });
